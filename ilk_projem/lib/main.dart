b83675c3bfe4fee8b29059ca4a686ecc425ffb7e
Berkay Topal <brkytpl@gmail.com>
2024-02-25 11:54:05 +0300

    mikinci dersim

--- ilk_projem/lib/main.dart
+++ ilk_projem/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  String mesaj = 'Mutlu bayramlar :)';
   runApp(MaterialApp(
-    home: Center(
-      child: Text('flutter çok eğlenceli'),
-    ),
-  ));
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey[100],
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[800],
+            title: Text(mesaj),
+          ),
+          body: Center(
+            child: Image.network(
+                'https://www.senveben.biz.tr/wp-content/uploads/2019/05/7796A158-7A4D-4BA1-B030-AD28183BEA2D-1024x1024.png'),
+          ))));
 }
