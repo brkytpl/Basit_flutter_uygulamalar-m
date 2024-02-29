efa077ce018cbb545d057a94e0b78f7c49dc2cbd
Berkay Topal <brkytpl@gmail.com>
2024-02-29 22:01:45 +0300

    Ãkahve uyg

---
+++ flutterkahvecisi/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    BenimUyg(),
+  );
+}
+
+class BenimUyg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(),
+      ),
+    );
+  }
+}
